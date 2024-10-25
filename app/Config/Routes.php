2683c1960b5e7778b8c6cf738f9de2d56e84aa22
LuisGabriel8615 <luis.gabriel.200615485@gmail.com>
2024-10-24 22:09:07 -0600

    2024 Proyectos - evaluación parcial II

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('inicio', 'Home::index');
+$routes->get('/', 'Home::index');
 $routes->get('Aplicacion', 'ProyController::Aplicaciones');
 $routes->get('frameweb', 'ProyController::Frame');
 $routes->get('Creditos', 'ProyController::Creditos');
